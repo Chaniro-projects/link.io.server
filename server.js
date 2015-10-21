8979defe786bc06783fd48f0fdad81772b7c2f01
Bastien Baret <bastien.baret@insa-rennes.fr>
2015-10-21 18:38:44 +0200

    Comments

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var http = require('http').Server();
 var io = require('socket.io')(http);
 var ConnectIO = require("./lib/connect.io.js")(io);
+
+//Default port
 var port = 8080;
 
-ConnectIO.bind();
+ConnectIO.start();
 
 http.listen(port, function(){
     console.log('Server started on *:' + port);
