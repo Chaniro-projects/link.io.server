4cde845e8e4a91e818a23c6983e6c531b21f7917
Bastien Baret <bastien.baret@insa-rennes.fr>
2015-10-22 14:50:41 +0200

    Rename API to Link.IO

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var http = require('http').Server();
 var io = require('socket.io')(http);
-var ConnectIO = require("./lib/connect.io.js")(io);
+var LinkIO = require("./lib/link.io.js")(io);
 
 //Default port
 var port = 8080;
 
-ConnectIO.start();
+LinkIO.start();
 
 http.listen(port, function(){
     console.log('Server started on *:' + port);
