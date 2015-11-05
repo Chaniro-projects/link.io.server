54ec6fec04fa457ac37ad6d2d93f760785af9813
Bastien Baret <bastien.baret@insa-rennes.fr>
2015-11-05 17:47:23 +0100

    Current log file as GET response

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
-var http = require('http').Server();
-var io = require('socket.io')(http);
-var LinkIO = require("./lib/link.io.js")(io);
+var http = require('http');
+var currentLogFile = "";
+var fs = require('fs');
 
-//Default port
+var server = http.createServer(function(req, res) {
+    fs.readFile('./log/' + currentLogFile, 'utf-8', function(error, content) {
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.end(content);
+    });
+
+});
+
 var port = 8080;
+
+var io = require('socket.io')(server);
+var LinkIO = require("./lib/link.io.js")(io, function(fileName) {
+    currentLogFile = fileName;
+});
 
 LinkIO.start();
 
-http.listen(port, function(){
+server.listen(port, function(){
     console.log('Server started on *:' + port);
 });
